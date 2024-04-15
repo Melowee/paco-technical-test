9fdfd804bc5da5b963a00ec25aba90dc42e60cd5
= <dauchezclement@live.fr>
2024-04-15 19:24:10 +0200

    createFlight endpoint ok

--- technical-test-api/src/main/java/technical/test/api/endpoints/FlightEndpoint.java
+++ technical-test-api/src/main/java/technical/test/api/endpoints/FlightEndpoint.java
@@ -1,10 +1,14 @@
 package technical.test.api.endpoints;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import technical.test.api.facade.FlightFacade;
 import technical.test.api.representation.FlightRepresentation;
 
@@ -17,5 +21,10 @@ public class FlightEndpoint {
     @GetMapping
     public Flux<FlightRepresentation> getAllFlights() {
         return flightFacade.getAllFlights();
+    }
+    
+    @PostMapping
+    public Mono<FlightRepresentation> createFlight(@RequestBody FlightRepresentation flight) {
+    	return flightFacade.createFlight(Mono.just(flight));
     }
 }
--- technical-test-api/src/main/java/technical/test/api/facade/FlightFacade.java
+++ technical-test-api/src/main/java/technical/test/api/facade/FlightFacade.java
@@ -1,12 +1,16 @@
 package technical.test.api.facade;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import technical.test.api.mapper.AirportMapper;
 import technical.test.api.mapper.FlightMapper;
 import technical.test.api.record.AirportRecord;
+import technical.test.api.record.FlightRecord;
 import technical.test.api.representation.FlightRepresentation;
 import technical.test.api.services.AirportService;
 import technical.test.api.services.FlightService;
@@ -14,22 +18,32 @@ import technical.test.api.services.FlightService;
 @Component
 @RequiredArgsConstructor
 public class FlightFacade {
-    private final FlightService flightService;
-    private final AirportService airportService;
-    private final FlightMapper flightMapper;
-    private final AirportMapper airportMapper;
+	private final FlightService flightService;
+	private final AirportService airportService;
+	private final FlightMapper flightMapper;
+	private final AirportMapper airportMapper;
 
-    public Flux<FlightRepresentation> getAllFlights() {
-        return flightService.getAllFlights()
-                .flatMap(flightRecord -> airportService.findByIataCode(flightRecord.getOrigin())
-                        .zipWith(airportService.findByIataCode(flightRecord.getDestination()))
-                        .flatMap(tuple -> {
-                            AirportRecord origin = tuple.getT1();
-                            AirportRecord destination = tuple.getT2();
-                            FlightRepresentation flightRepresentation = this.flightMapper.convert(flightRecord);
-                            flightRepresentation.setOrigin(this.airportMapper.convert(origin));
-                            flightRepresentation.setDestination(this.airportMapper.convert(destination));
-                            return Mono.just(flightRepresentation);
-                        }));
+	public Flux<FlightRepresentation> getAllFlights() {
+		return flightService.getAllFlights()
+				.flatMap(flightRecord -> airportService.findByIataCode(flightRecord.getOrigin())
+						.zipWith(airportService.findByIataCode(flightRecord.getDestination())).flatMap(tuple -> {
+							AirportRecord origin = tuple.getT1();
+							AirportRecord destination = tuple.getT2();
+							FlightRepresentation flightRepresentation = this.flightMapper.convert(flightRecord);
+							flightRepresentation.setOrigin(this.airportMapper.convert(origin));
+							flightRepresentation.setDestination(this.airportMapper.convert(destination));
+							return Mono.just(flightRepresentation);
+						}));
+	}
+
+	public Mono<FlightRepresentation> createFlight(Mono<FlightRepresentation> flight) {
+    	return flight.flatMap(flightRepresentation -> {
+    		flightRepresentation.setId(UUID.randomUUID());
+    		FlightRecord flightRecord = this.flightMapper.convert(flightRepresentation);
+    		return this.flightService.createFlight(flightRecord).flatMap(insertedFlightRecord -> {
+    			FlightRepresentation insertedFlightRepresentation = this.flightMapper.convert(insertedFlightRecord);
+    			return Mono.just(insertedFlightRepresentation);
+    		});    		
+    	});
     }
 }
